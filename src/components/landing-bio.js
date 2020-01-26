6a75402d9e7030e0164f3a1a8ff5c15e29ec1762
Chris Marlow <christopher.marlow@shopify.com>
2020-01-26 15:26:25 -0500

    Cleaning up components

--- src/components/landing-bio.js
+++ src/components/landing-bio.js
@@ -1,18 +1,14 @@
 import React from "react"
-import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 import styled from "@emotion/styled"
-
-const Container = styled.div`
-  text-align: center;
-`
+import { Content } from "./layout"
 
 const OuterContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: row;
-  height: 78vh;
+  min-height: 78vh;
 `
 
 const Description = styled.p`
@@ -40,21 +36,11 @@ export const LandingBio = () => (
     `}
     render={data => (
       <OuterContainer>
-        <Container>
+        <Content>
           <NameHeader>{data.site.siteMetadata.title}</NameHeader>
           <Description>{data.site.siteMetadata.subtitle}</Description>
-        </Container>
+        </Content>
       </OuterContainer>
     )}
   />
 )
-
-NameHeader.propTypes = {
-  siteTitle: PropTypes.string,
-  subtitle: PropTypes.string,
-}
-
-NameHeader.defaultProps = {
-  siteTitle: ``,
-  subtitle: ``,
-}
